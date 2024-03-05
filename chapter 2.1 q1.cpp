e936912cb0461cec17b3114094a3c4502bddec8e
0DHARMIK <162312883+0DHARMIK@users.noreply.github.com>
2024-03-05 16:48:53 +0530

    Add files via upload

---
+++ chapter 2.1 q1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+
+{  
+   int a=12,b=6;
+   
+    printf("Addition of %d and %d = %d\n",a,b,a+b);
+    printf(" %d + %d = %d\n",a,b,a+b);
+    printf(" %d - %d = %d\n",a,b,a-b);
+    printf(" %d * %d = %d\n",a,b,a*b);
+    printf(" %d / %d = %d\n",a,b,a/b);
+    printf(" %d %%  %d = %d\n",a,b,a%b);
+    
+    
+}
